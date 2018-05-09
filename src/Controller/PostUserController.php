41e9cdbdb4a24fac8e025f85432d281b5cc7f5e4
pnonell <pau.nonell@gmail.com>
2018-05-09 20:05:56 +0200

    comprovacio registre Front

--- src/Controller/PostUserController.php
+++ src/Controller/PostUserController.php
@@ -2,6 +2,7 @@
 
 namespace Pwbox\Controller;
 
+use Doctrine\DBAL\Driver\Mysqli\MysqliException;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -31,34 +32,34 @@ class PostUserController
 
     public function registerAction(Request $request, Response $response)
     {
-        try{
+        try {
 
 
             $data = $request->getParsedBody();
 
             //password
-            if (!(strlen($data['password']) > 5 and strlen($data['password']) < 13  and
+            if (!(strlen($data['password']) > 5 and strlen($data['password']) < 13 and
                 preg_match('/[a-z]/', $data['password']) and preg_match('/[A-Z]/', $data['password'])
-                and preg_match('/[0-9]/', $data['password']))){
+                and preg_match('/[0-9]/', $data['password']))) {
 
                 return $this->container->get('view')
                     ->render($response, 'register.twig', ['error' => "contraseña con formato incorrecto"]);
             }
 
             //confirm password
-            if (strcmp($data['password'], $data['confirm_password']) != 0){
+            if (strcmp($data['password'], $data['confirm_password']) != 0) {
                 return $this->container->get('view')
                     ->render($response, 'register.twig', ['error' => "las dos contrasenyas no son iguales"]);
             }
 
             //username
-            if(!(ctype_alnum($data['username']) and strlen($data['username']) > 0 and strlen($data['username']) < 21)){
+            if (!(ctype_alnum($data['username']) and strlen($data['username']) > 0 and strlen($data['username']) < 21)) {
                 return $this->container->get('view')
                     ->render($response, 'register.twig', ['error' => "nombre de usuario con formato incorrecto"]);
             }
 
             //email
-            if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
                 return $this->container->get('view')
                     ->render($response, 'register.twig', ['error' => "correo con formato incorrecto"]);
             }
@@ -67,17 +68,14 @@ class PostUserController
             $_SESSION["user_id"] = $service($data);
 
 
-
-
             $this->container->get('flash')->addMessage('dashboard', 'User registered.');
-
 
 
             // create account verification link
             $link = 'http://' . $_SERVER['SERVER_NAME'] . '/activation.php?key=' . 'EXAMPLE';
 
             // get the html email content
-            $directory = __DIR__.'/../view/emails/';
+            $directory = __DIR__ . '/../view/emails/';
             $html_content = file_get_contents($directory . 'email_verification.html');
             echo $html_content;
             /*$html_content = preg_replace('/{link}/', $link, $html_content);*/
@@ -105,8 +103,7 @@ class PostUserController
             // Create the Transport
             $transport = (new \Swift_SmtpTransport($smtp_server, $port))
                 ->setUsername($username)
-                ->setPassword($password)
-            ;
+                ->setPassword($password);
 
             // Create the Mailer using your created Transport
             $mailer = new \Swift_Mailer($transport);
@@ -114,18 +111,10 @@ class PostUserController
             $mailer->send($message);
 
 
-
-
-
-
-
-
-
-
             return $response->withStatus(302)->withHeader('Location', '/dashboard');
         } catch (\Exception $e){
             return $this->container->get('view')
-                ->render($response, 'register.twig', ['error' => $e->getMessage()]);
+                ->render($response, 'register.twig', ['error' => 'code: '.$e->getMessage()]);
         }
         return $response;
     }
