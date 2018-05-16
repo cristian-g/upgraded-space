802c2d7bd2ad1c0296296470d0ef08a0f09d8018
Marc Castells i Güell <marc.castells.guell@gmail.com>
2018-05-16 17:00:58 +0200

    solucionat error amb les imatges de perfil

--- src/Model/UseCase/PostUserUseCase.php
+++ src/Model/UseCase/PostUserUseCase.php
@@ -1,15 +1,11 @@
 <?php
-
 namespace Pwbox\Model\UseCase;
-
 use Pwbox\Model\User;
 use Pwbox\Model\UserRepository;
-
 class PostUserUseCase
 {
     /** @var UserRepository */
     private $repository;
-
     /**
      * PostUserUseCase constructor.
      * @param UserRepository $repository
@@ -18,9 +14,7 @@ class PostUserUseCase
     {
         $this->repository = $repository;
     }
-
-
-    public function __invoke(array $rawData)
+    public function __invoke(array $rawData, $value, $extension)
     {
         $now = new \DateTime('now');
         $user = new User(
@@ -32,7 +26,9 @@ class PostUserUseCase
             $rawData['password'],
             0,
             $now,
-            $now
+            $now,
+            $value,
+            $extension
         );
         return $this->repository->save($user);
     }
