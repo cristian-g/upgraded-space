f532d064eab8d55f22acb56b1785bfbebb1e80bd
pnonell <pau.nonell@gmail.com>
2018-05-16 10:14:28 +0200

    Share Middleware

--- src/Controller/Middleware/ShareMiddleware.php
+++ src/Controller/Middleware/ShareMiddleware.php
@@ -17,30 +17,14 @@ class ShareMiddleware
 
     public function __invoke(Request $request, Response $response, callable $next)
     {
-//        if (!isset($_SESSION["user_id"])) {
-//            return $response->withStatus(302)->withHeader('Location', '/login');
-//        }else{
-//            if($_POST['user_id'] != $_SESSION["user_id"]){
-//                return $response->withStatus(302)->withHeader('Location', '/logout');
-//            }
-//        }
-      //  $folder = ($this->container->get('get_folder_by_uuid_use_case'))($args['uuid']);
-
         // Role
-     /*   if ($folder->getIdUser() == $_SESSION["user_id"]) {
-            $role = 'owner';
-        }*/
-        // Role
-            $folder = ($this->container->get('get_folder_by_uuid_use_case'))($_POST['uuid_upload']);
-//        var_dump($folder);
-//        die();
-            $role = null;
-            $share = RoleCalculator::computeRole($folder, $role, $this->container);
+        $folder = ($this->container->get('get_folder_by_uuid_use_case'))($_POST['uuid_upload']);
 
-       /* $file = __DIR__ . "/log.txt";
-        var_dump($_POST['uuid_upload']);
-die();
-        file_put_contents($file, $content);*/
+        $role = null;
+        $share = RoleCalculator::computeRole($folder, $role, $this->container);
+        if($role != 'owner'){
+            return $response->withStatus(302)->withHeader('Location', '/403');
+        }
         return $next($request, $response);
     }
 }
